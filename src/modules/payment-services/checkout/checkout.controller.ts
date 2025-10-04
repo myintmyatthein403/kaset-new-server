88f44d0918267a6c9a205856e0fa72218d0e9deb
Myint Myat Thein <myintmyatthein403@gmail.com>
2025-10-04 08:00:32 +0700

    add checkout api

--- src/modules/payment-services/checkout/checkout.controller.ts
+++ src/modules/payment-services/checkout/checkout.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Post, Query } from '@nestjs/common';
 import { CheckoutService } from './checkout.service';
-import { CreateCheckoutDto } from './dto/create-checkout.dto';
-import { UpdateCheckoutDto } from './dto/update-checkout.dto';
 
 @Controller('checkout')
 export class CheckoutController {
   constructor(private readonly checkoutService: CheckoutService) { }
 
-  @Get('/callback')
+  @Post('/callback')
   getCallback(
     @Query() data: any
   ) {
